df177df2f9e3c5ec07048ecd2d2465df6c895907
rickoshi <145235121+rickoshi@users.noreply.github.com>
2023-09-17 06:08:51 -0300

    versão final do exemplo 1, Deu certo!!!

--- LP-01/exemplo01.c
+++ LP-01/exemplo01.c
@@ -1,7 +1,15 @@
+/* OBESO.C - determina se uma pessoa está obesa */
+
 #include <stdio.h>
 #include <math.h>
 
 int main(void) {
-    printf("olá");
-    return 0;
+   float peso, altura, imc;
+   printf("Qual o peso e a altura? ");
+   scanf("%f %f", &peso, &altura);
+   imc = peso/pow(altura,2);
+   printf("IMC = %.1f\n",imc);
+   if( imc<=30 ) printf("Nao esta obesa!\n");
+   else printf("Esta obesa!\n");
+   return 0;
 }
