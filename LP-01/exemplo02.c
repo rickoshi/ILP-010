b7f9fd71f47e50734a7b77e6eacffd856bfab052
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-01-27 00:53:16 -0300

    exemplo 2 final

--- LP-01/exemplo02.c
+++ LP-01/exemplo02.c
@@ -11,14 +11,36 @@ char tecla = 'A', opcao, numero = '2';
 int x = 07, y = 3, z, a = 0x7F; // 07 é octal e ox7F é hexadec
 float comissao = 0.10, desconto, salario, teste = 5.67e2;
 
-
 // Não há o tipo string, mas há constantes desse tipo
 
 // Os dados podem se apresentar em duas formas distintas: como constantes ou variáveis (que variam de uma execução para outra) 
 
-// caracter a uma variável = seu código ASCII. Adicionar '', ch = 'A'
+// char = seu código ASCII. Adicionar '' para char, ch = 'A'
 
 // Uma declaração consiste em um tipo e um identificador. 
 // tipo = o espaço de memória , identificador = referência (inicia por letra).
 
 // Uma variável pode ou não ser iniciada ao ser declarada
+
+
+
+/*
+Tipo modificado         Bytes       Escala
+unsigned char           1           0 ... 255
+unsigned int            4           0 ... 4294967295
+short int               2       −32767 ... +32767
+long long int           8       −922337203e10 ... +922337203e10
+unsigned short int      2           0 ... 65535
+unsigned long long int  8           0 ... 18446744073709551615
+*/
+
+unsigned char contador;
+unsigned int a, b, c;
+long int tam_arquivo;
+
+// O bit mais à esquerda (bit de sinal) no tipo char ou int é utilizado para distinguir entre valores positivos e negativos: bit zero = valor é positivo; caso contrário, negativo. 
+
+// Usando o modificador unsigned, somente valores sem sinal serão usados e a escala de valores dobra. 
+
+// O modificador long faz com que o espaço de memória reservado para uma variável do tipo int seja duplicado. 
+// O modificador short faz com que esse espaço caia para a metade.
