7a03aa1fd5279103426d67ae80e07ec00018b419
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-02-03 06:41:51 -0300

    Exercício 5. ASCII

--- LP-01/exercícios/exercicio05.c
+++ LP-01/exercícios/exercicio05.c
@@ -2,13 +2,13 @@
 // Dado um caractere, exiba o código ASCII correspondente
 
 #include <stdio.h>
-int main(void) {
-char ascii = 'A'; // tipo char é iniciado em aspas simples
-printf("%c \n", ascii); // A
-printf("%d \n", ascii); // 65
-// Quando uma variável é declarada no tipo char, a linguagem C entende a var. como sendo o código ASCII para ser representado visualmente. Sendo assim, se convertido de char (visual) para int (código), o dado de saída será o prório código ASCII da variável.
+    int main(void) {
+    char ascii = 'A'; // tipo char é iniciado em aspas simples
+    printf("%c \n", ascii); // A
+    printf("%d \n", ascii); // 65
+    // Quando uma variável é declarada no tipo char, a linguagem C entende a var. como sendo o código ASCII para ser representado visualmente. Sendo assim, se convertido de char (visual) para int (código), o dado de saída será o prório código ASCII da variável.
 
-char caractere;
-scanf("%c", &caractere);    // a
-printf("%d \n", caractere); // 97
+    char caractere;
+    scanf("%c", &caractere);    // a
+    printf("%d \n", caractere); // 97
 }
