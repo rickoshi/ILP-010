f99b2461bc2b06d9ede56c53fd44be3982c90f48
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-02-11 07:01:49 -0300

    Exercício 3. Tabela-verdade do operador !

---
+++ LP-02/exercícios/exercicio03.c
@@ -0,0 +1,11 @@
+// Exercício 3. Tabela-verdade do operador !
+// Crie um programa para exibir a tabela-verdade do operador !.
+
+#include <stdio.h>
+int main(void) {
+printf(" 0 == %d\n", 0);    // 0
+printf("!0 == %d\n",!0);    // 1
+printf(" 1 == %d\n", 1);    // 1
+printf("!1 == %d\n",!1);    // 0
+return 0;
+}
