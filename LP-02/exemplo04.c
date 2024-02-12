dd58c4a5afbfcbe19edf6b84491abf8f883a80df
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-02-12 04:34:30 -0300

    O comando if-else

--- LP-02/exemplo04.c
+++ LP-02/exemplo04.c
@@ -4,12 +4,12 @@
 // Exemplo 4. O comando if-else
 #include <stdio.h>
 int main(void) {
-int idade;
-printf("Idade? ");
-scanf("%d",&idade);
-if( idade<=18 ) puts("Menor");  // Se a idade for menor ou igual a 18, executará o comando-1
-else puts("Maior");  // Senão, executa o comando-2
-return 0;
+    int idade;
+    printf("Idade? ");
+    scanf("%d",&idade);
+    if( idade<=18 ) puts("Menor");  // Se a idade for menor ou igual a 18, executará o comando-1
+    else puts("Maior");  // Senão, executa o comando-2
+    return 0;
 }
 
 /*
