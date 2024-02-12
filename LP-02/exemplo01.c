798fe7b70158213c3bd6a4017a5d495e95fb8fdc
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-02-12 04:34:07 -0300

    O tipo bool

--- LP-02/exemplo01.c
+++ LP-02/exemplo01.c
@@ -11,7 +11,7 @@ A constante 0 representa falso e todo valor diferente de 0 representa verdade.
 #include <stdio.h>
 #include <stdbool.h>
 int main(void) {
-bool p = false, q = 7;
-printf("sizeof(bool) = %d, p = %d, q = %d\n",sizeof(bool),p,q); // 1 0 1
-return 0;
+    bool p = false, q = 7;
+    printf("sizeof(bool) = %d, p = %d, q = %d\n",sizeof(bool),p,q); // 1 0 1
+    return 0;
 }
